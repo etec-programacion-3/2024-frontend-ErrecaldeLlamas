ab0beda866412d585a44983405a242ae8cc55a82
NaiErre <n.errecalde@alumno.etec.um.edu.ar>
2024-10-25 12:54:19 -0300

    creacion de componentes order y order-items falta asignarles las rutas

---
+++ angular-ecommerce/src/app/components/order-items/order-items.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-items',
+  standalone: true,
+  imports: [],
+  templateUrl: './order-items.component.html',
+  styleUrl: './order-items.component.css'
+})
+export class OrderItemsComponent {
+
+}
